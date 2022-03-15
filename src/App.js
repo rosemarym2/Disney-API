527fa304fd6575fc87e3c746d0ddb6d3050b9c34
rosemarym2 <rosemarymay91@gmail.com>
2022-03-15 18:59:49 +0000

    Disney-API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import './App.css';
 
 
